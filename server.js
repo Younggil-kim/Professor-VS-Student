4997e1ea3d3d709574428178292e2af968d06211
김영길 <gom991@ajou.ac.kr>
2021-05-06 18:18:06 +0900

    불필요한 코드 수정

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
-
+//server.js
+const app = require('http').createServer(handler);
+const io = require('socket.io')(app);
+const fs = require('fs');
+//8000번 포트에서 서버 실행
 app.listen(8000);
 
+//index.html파일을 읽어오는 코드
 function handler (req, res) {
     fs.readFile(__dirname + '/views/index.html', function( err, data) {
         if(err){
@@ -15,23 +17,11 @@ function handler (req, res) {
     });
 }
 
-//플레이어 색상 랜덤값으로 지
+//플레이어 색상 랜덤값으로 지정
 function getPlayerColor(){
-    let colorMax = 16711680;
-    let colorMin = 255;
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
-    //최소값 255, 최대값 16,711,680
 }
 
-class InputData{
-    constructor(num){
-        this.num = num;
-        this.up = false;
-        this.down = false;
-        this.left = false;
-        this.right = false;
-    }
-}
 
 const startX = 1024/2;
 const startY = 768/2;
@@ -39,8 +29,8 @@ const startY = 768/2;
 class PlayerBall{
     constructor(socket){
         this.socket = socket;
-        this.x = Math.random()*1024;
-        this.y = Math.random()*768;
+        this.x = startX;
+        this.y = startY;
         this.color = getPlayerColor();
     }
 
@@ -68,7 +58,6 @@ function endGame(socket){
             break
         }
     }
-
     delete ballMap[socket.id];
 }
 
@@ -76,16 +65,15 @@ io.on('connection', function(socket) {
     console.log(`${socket.id}님이 입장하셨습니다.`);
 
     socket.on('disconnect', function(reason){
-        console.log(`${socket.id}님이 퇴장하셨습니다. `)
+        console.log(`${socket.id}님이 ${reason}의 이유로 퇴장하셨습니다. `)
         endGame(socket);
         socket.broadcast.emit('leave_user', socket.id);
     });
 
     let newBall = joinGame(socket);
-
     socket.emit('user_id', socket.id);
 
-    // 이미 있는 유저들에게 데이터 보내기
+    // 이미 있는 유저들의 데이터 보내기
     for (var i = 0 ; i < balls.length; i++){
         let ball = balls[i];
         socket.emit('join_user', {
@@ -95,7 +83,7 @@ io.on('connection', function(socket) {
             color: ball.color,
         });
     }
-    // 새로운 유저에게 데이터 보내기
+    // 새로운 유저의 데이터 보내기
     socket.broadcast.emit('join_user',{
         id: socket.id,
         x: newBall.x,
@@ -105,48 +93,11 @@ io.on('connection', function(socket) {
 
     //유저에게 변경사항 보내기
     socket.on('send_location', function(data) {
-        console.log(data);
             socket.broadcast.emit('update_state', {
                 id: data.id,
                 x: data.x,
                 y: data.y,
             })
     })
-
-
-
 })
 
-var previousUpdateTime = new Date().getTime();
-
-
-
-
-function updateGame(){
-    let currentUpdateTime = new Date().getTime();
-    previousUpdateTime = currentUpdateTime;
-    setTimeout(updateGame, 16);
-}
-
-function broadcastState(datas){
-    
-    let data = {};
-    if (datas != null){
-        // console.log("here",datas);        
-        for(var i = 0 ; i < balls.length ; i++ ){
-            let ball = balls[i];
-            data[ball.id] = {
-                last_input_num: ball.lastInputNum,
-                x: datas[ball].x,
-                y: datas[ball].y,
-            };
-            // console.log("1",data);
-        }
-    }
-    io.sockets.emit('update_state', data);
-
-    setTimeout(broadcastState, 33);
-}
-
-updateGame();
-broadcastState();
