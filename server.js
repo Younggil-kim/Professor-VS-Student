760b794dff6fac4e2bc2d8b5bf41559e6984bebf
김영길 <gom991@ajou.ac.kr>
2021-05-09 21:10:21 +0900

    적 생성과 움직임

--- server.js
+++ server.js
@@ -1,30 +1,55 @@
 //server.js
-const app = require('http').createServer(handler);
-const io = require('socket.io')(app);
-const fs = require('fs');
+// const app = require('http').createServer(handler);
+const app = require('express')();
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+// const fs = require('fs');
 //8000번 포트에서 서버 실행
-app.listen(8000);
+// app.listen(8000);
+
+server.listen(8000, () => {
+    console.log("서버가 대기중입니다.");
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/views/room.html')
+
+// })
 
 //index.html파일을 읽어오는 코드
-function handler (req, res) {
-    fs.readFile(__dirname + '/views/index.html', function( err, data) {
-        if(err){
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
-}
+// function handler (req, res) {
+//     fs.readFile(__dirname + '/views/index.html', function( err, data) {
+//         if(err){
+//             res.writeHead(500);
+//             return res.end('Error loading index.html');
+//         }
+//         res.writeHead(200);
+//         res.end(data);
+//     });
+// }
 
 //플레이어 색상 랜덤값으로 지정
 function getPlayerColor(){
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
+const canvasWidth = 1024;
+const canvasHeight = 768;
 
-const startX = 1024/2;
-const startY = 768/2;
+const startX = canvasWidth/2;
+const startY = canvasHeight/2;
+
+var enemyStartX = 0;
+var enemyStartY = 0;
+var enemyDestinationX = 0;
+var enemyDestinationY = 0;
+var enemyRadius = 10;
 
 class PlayerBall{
     constructor(socket){
@@ -36,6 +61,19 @@ class PlayerBall{
 
     get id() {
         return this.socket.id;
+    }
+}
+// var enemyStartX = enemyRadius;
+// var enemyStartY = Math.floor(Math.random() * 768);
+
+var enemySpeedX = 0;
+var enemySpeedY = 0;
+var enemyAliveTime = 400;
+
+class EnemyLeftBall{
+    constructor(){
+        this.x = enemyRadius;
+        this.y = Math.floor(Math.random() * 768);
     }
 }
 
@@ -61,18 +99,23 @@ function endGame(socket){
     delete ballMap[socket.id];
 }
 
+
+
+
 io.on('connection', function(socket) {
     console.log(`${socket.id}님이 입장하셨습니다.`);
-
     socket.on('disconnect', function(reason){
         console.log(`${socket.id}님이 ${reason}의 이유로 퇴장하셨습니다. `)
         endGame(socket);
-        socket.broadcast.emit('leave_user', socket.id);
+        io.sockets.emit('leave_user', socket.id);
     });
+
+    // socket.on('move', function(){
+    //     socket.broadcast.emit('move_gameroom');
+    // })
 
     let newBall = joinGame(socket);
     socket.emit('user_id', socket.id);
-
     // 이미 있는 유저들의 데이터 보내기
     for (var i = 0 ; i < balls.length; i++){
         let ball = balls[i];
@@ -83,6 +126,7 @@ io.on('connection', function(socket) {
             color: ball.color,
         });
     }
+
     // 새로운 유저의 데이터 보내기
     socket.broadcast.emit('join_user',{
         id: socket.id,
@@ -99,5 +143,61 @@ io.on('connection', function(socket) {
                 y: data.y,
             })
     })
+
+
+    setInterval(function() {
+        if(balls.length){
+            //5초정도 있다가 시작하게 바꾸기. 
+            let host = balls[0].id;
+            var decideWall = Math.floor(Math.random()*4);
+            if( decideWall == 0){
+                var randomStartY = Math.floor(Math.random() * 768)
+                var randomdetiY = Math.floor(Math.random() * 768)
+                io.sockets.emit('enemy_left', {
+                    wall : 0,
+                    startingX:  enemyRadius,
+                    startingY:  randomStartY,
+                    destinationX:  canvasWidth+enemyRadius,
+                    destinationY: randomdetiY,
+                })
+            }
+            else if( decideWall == 1){
+                var randomStartY = Math.floor(Math.random() * 768)
+                var randomdetiY = Math.floor(Math.random() * 768)
+                io.sockets.emit('enemy_left', {
+                    wall : 1,
+                    startingX:  canvasWidth+enemyRadius,
+                    startingY:  randomStartY,
+                    destinationX:  enemyRadius,
+                    destinationY: randomdetiY,
+                })
+            }
+            else if( decideWall == 2){
+                var randomStartX = Math.floor(Math.random() * 1024);
+                var randomdetiX = Math.floor(Math.random() * 1024);
+                io.sockets.emit('enemy_left', {
+                    wall : 2,
+                    startingX:  randomStartX,
+                    startingY:  enemyRadius,
+                    destinationX:  randomdetiX,
+                    destinationY: canvasHeight+enemyRadius,
+                })
+            }
+            else if( decideWall == 3){
+                var randomStartX = Math.floor(Math.random() * 1024);
+                var randomdetiX = Math.floor(Math.random() * 1024);
+                io.sockets.emit('enemy_left', {
+                    wall : 3,
+                    startingX:  randomStartX,
+                    startingY:  canvasHeight+enemyRadius,
+                    destinationX:  randomdetiX,
+                    destinationY: enemyRadius,
+                })
+            }
+
+    }
+    }, 1000)
+
+
 })
 
