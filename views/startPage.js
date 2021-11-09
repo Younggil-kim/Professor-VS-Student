1ee7527cbd61bdcf82a8e1e5481e35ea6cc0b5ea
Younggil-kim <gom991@ajou.ac.kr>
2021-11-10 02:06:23 +0900

    Css

--- views/startPage.js
+++ views/startPage.js
@@ -1,6 +1,21 @@
 function enterGame(){
     var enter = document.getElementById('enter').value
-    
-    localStorage.setItem("nickName", enter);
-    location.href= "/"
+    if(enter.length === 0){
+        Swal.fire({
+            title: "게임 알림",
+            text: "닉네임을 입력해주세요",
+            confirmButtonText: "네"
+        });
+    }else if( enter.length < 2 || enter.length > 9 ){
+        Swal.fire({
+            title: "게임 알림",
+            text: "닉네임은 최소 2자에서 최대 8자 입니다.",
+            confirmButtonText: "네"
+        });
+    }else{
+        localStorage.setItem("nickName", enter);
+        location.href= "/"
+    }
+
+
 }
